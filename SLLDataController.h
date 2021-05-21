e8a2d8f6fb4ff8473a2d95e3a536ceb766d66f3d
dathoanh <dathoanh99@gmail.com>
2021-05-21 22:59:45 +0700

    Update source code

--- SLLDataController.h
+++ SLLDataController.h
@@ -2,7 +2,7 @@
 //  SLLDataControler.h
 //  KTLT ASS 3
 //
-//  Created by Nhân Nguyễn on 17/05/2021.
+//  Completed by Đào Hoành Đạt on 21/05/2021.
 //
 
 // Students can remove the final initial return of the functions
@@ -13,6 +13,8 @@
 #include "dataStructure.h"
 #include <vector>
 #include <string>
+
+SoldierNode* getSoldier(const SLinkedList& list, const int pos);
 
 //Functions used to manage Singly Linked List
 void print(SLinkedList& list){
@@ -32,22 +34,101 @@ bool insertAt (SLinkedList& list, Soldier element, int pos){
     //Insert element into a pos in the SLL
     //Return true if insert successfully, false otherwise
     //TODO
+
+    if(pos < 0 || pos > list.size) return false;
+    SoldierNode *newNode = new SoldierNode(element, NULL);
+    if(newNode == NULL) return false;
+
+    if(list.size == 0)
+    {
+        list.head = newNode;
+    }
+    else
+    {
+        SoldierNode *prevNode, *currNode;
+        currNode = list.head;
+        int index = 0;
+        while(index < pos)
+        {
+            index++;
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+
+        prevNode->next = newNode;
+        newNode->next = currNode;
+
+    }
+
+    list.size++;
     
-    return false;
+    return true;
 }
 
 bool removeAt (SLinkedList& list, int idx){
     //Remove element at index idx in the SLL
     //Return true if remove successfully, false otherwise
     //TODO
+
+    if(idx < 0 || idx >= list.size) return false;
+
+    SoldierNode *prevNode, *currNode;
+    currNode = list.head;
+    int index = 0;
+    if(idx == 0)
+    {
+        list.head = currNode->next;
+    }
+    else
+    {
+        while(index < idx)
+        {
+            index++;
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+        prevNode->next = currNode->next;
+    }
     
-    return false;
+    currNode->next = NULL;
+
+    delete currNode;
+    list.size--;
+
+    return true;
 }
 
 bool removeFirstItemWithHP (SLinkedList& list, int HP){
     //Remove the first element with HP equals to HP
     //Return true if remove successfully, false otherwise
     //TODO
+
+    if(list.size == 0) return false;
+
+    SoldierNode *prevNode, *currNode;
+    currNode = list.head;
+
+    while(currNode != NULL)
+    {
+        if(currNode->data.HP == HP)
+        {
+            if(currNode == list.head)
+            {
+                list.head = currNode->next;
+            }
+            else
+            {
+                prevNode->next = currNode->next;
+            }
+
+            currNode->next = NULL;
+            delete currNode;
+            list.size--;
+            return true;
+        }
+        prevNode = currNode;
+        currNode = currNode->next;
+    }
     
     return false;
 }
@@ -56,8 +137,26 @@ int indexOf(SLinkedList& list, Soldier soldier){
     //Find index of soldier (start from 0)
     //Return -1 if the soldier does not exist
     //TODO
+
+    if(list.size == 0) return -1;
+
+    SoldierNode *tempNode = list.head;
+    int index = 0;
+    while(tempNode != NULL)
+    {
+        if(tempNode->data.ID == soldier.ID)
+        {
+            if( tempNode->data.HP == soldier.HP &&
+                tempNode->data.isSpecial == soldier.isSpecial)
+                {
+                    return index;
+                }
+        }
+        index++;
+        tempNode = tempNode->next;
+    }
     
-    return -2;
+    return -1;
     
 }
 
@@ -65,34 +164,60 @@ int size(SLinkedList& list){
     //Return size of the list
     //TODO
     
-    return -1;
+    return list.size;
 }
 
 bool empty(SLinkedList& list){
     //Check whether the list is empty
     //TODO
     
-    return false;
+    if(list.size == 0) 
+        return true;
+    else 
+        return false;
 }
 
 void clear(SLinkedList& list){
     //Delete all of the elements in list
     //TODO
     
+    SoldierNode *delNode;
+
+    while(list.head != NULL)
+    {
+        delNode = list.head;
+        list.head = delNode->next;
+
+        delNode->next = NULL;
+
+        delete delNode;
+        list.size--;
+    }
+
 }
 
 string getIDAt(SLinkedList& list, int pos){
     //Get ID of the Soldier at pos
     //TODO
+
+    if(pos < 0 || pos >= list.size) return "-1";
+
+    SoldierNode *tempNode;
+    tempNode = getSoldier(list, pos);
     
-    return "";
+    return tempNode->data.ID;
 }
 
 int getHPAt(SLinkedList& list, int pos){
     //Get HP of the Soldier at pos
     //TODO
+
+    if(pos < 0 || pos >= list.size) return -1;
+
+    SoldierNode *tempNode;
+    tempNode = getSoldier(list, pos);
     
-    return -1;
+    return tempNode->data.HP;
 }
 
 bool setHPAt(SLinkedList& list, int HP, int pos){
@@ -100,18 +225,42 @@ bool setHPAt(SLinkedList& list, int HP, int pos){
     //TODO
     //Return true if set successfully, false otherwise
     
-    return false;
+    if(pos < 0 || pos >= list.size) return false;
+
+    SoldierNode *tempNode;
+    tempNode = getSoldier(list, pos);
+    tempNode->data.HP = HP;
+
+    return true;
 }
 
 bool contains (SLinkedList& list, Soldier soldier){
     //Check if array contains soldier
     //TODO
-    
-    return false;
+
+    int index = indexOf(list, soldier);
+
+    if(index == -1)
+        return false;
+    else 
+        return true;
 }
 
 //You can write your own functions here
+SoldierNode* getSoldier(const SLinkedList& list, const int pos)
+{
+    SoldierNode *retNode;
+    retNode = list.head;
+    int index = 0;
 
+    while(index < pos)
+    {
+        index++;
+        retNode = retNode->next;
+    }
+
+    return retNode;
+}
 //End your own functions
 
 #endif /* SLLDataControler_h */
